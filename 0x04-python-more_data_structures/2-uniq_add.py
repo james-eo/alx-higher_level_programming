6b12841c23e8428f312c60a31ac10f9787ec1719
james-eo <jameseokonkwo@gmail.com>
2023-01-11 23:22:32 +0100

    Function that adds unique intergers in a list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    add = 0
-
-    for i in set(my_list):
-       add += i
-    return (add)
+    new = set(my_list)
+    result = 0
+    for i in new:
+        result += i
+    return (result)
