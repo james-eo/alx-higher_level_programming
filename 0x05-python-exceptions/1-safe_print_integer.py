aafda9a59beb618ec63408b8c3a7bf47cd9ffae7
james-eo <jameseokonkwo@gmail.com>
2023-12-18 22:08:09 +0100

    Print integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        formatted_value = "{:d}".format(value)
+        formatted_value = "{:d}".format(int(value))
         print(formatted_value, end="")
         print()
         return True
-    except:
+    except ValueError:
         return False
