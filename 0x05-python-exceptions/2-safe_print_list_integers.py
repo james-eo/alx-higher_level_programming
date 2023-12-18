d8fa9161c8de958ac448e118927b5cfe9c8fb830
james-eo <jameseokonkwo@gmail.com>
2023-12-18 22:42:32 +0100

    Print save integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,6 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             num += 1
-            print()
         except (ValueError, TypeError):
             continue
     print()
