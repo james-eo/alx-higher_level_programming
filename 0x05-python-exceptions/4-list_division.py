299e606a7cba94a24af08010f1dbbaf6498ccb07
james-eo <jameseokonkwo@gmail.com>
2023-12-18 23:32:23 +0100

    Divide element 1 by element 2 lists.

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,7 +2,7 @@
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
 
-    for i in range(0, list_length):
+    for i in range(list_length):
     	result = 0
         try:
             result = my_list_1[i] / my_list_2[i]
@@ -12,8 +12,6 @@ def list_division(my_list_1, my_list_2, list_length):
             print("division by 0")
         except (IndexError):
             print("out of range")
-
         finally:
             new_list.append(result)
-
     return (new_list)
