feb11347bb5825f65bf577a6b4dd751bf7170ba8
james-eo <jameseokonkwo@gmail.com>
2024-01-10 12:12:42 +0100

    Test everything

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+	"""
+	This function adds two integers.
+	
+	
+	Args:
+		a (int or float): The first number
+		b (int or flaot): The second number with default = 98.
+		
+	
+	Rewturns:
+	
+		int: the sum a and b
+		
+	Raises:
+		TypeError if a or b is not int or flaot
+	"""
+	
+	if not isinstance(a, (int, float)):
+		raise TypeError("a must be an integer")
+	if not isinstance(b, (int, float)):
+		raise TypeError("b must be an integer")
+		
+	return int(a) + int(b)
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,8 @@
+0x07-python-test_driven_development
+
+doctest — Test interactive Python examples (until “26.2.3.7. Warnings” included)
+doctest – Testing through documentation
+Unit Tests in Python
+Unittest module
+Interactive and Non-interactive tests
+
