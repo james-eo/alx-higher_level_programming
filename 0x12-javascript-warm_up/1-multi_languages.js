6343598b084125b503e5c479910a39224f902ae2
james-eo <jameseokonkwo@gmail.com>
2024-03-11 12:35:16 +0100

    Multilanguages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const myC = 'C is fun';
+const myPython = 'Python is cool';
+const myJS = 'JavaScript is amazing';
+
+console.log(myC);
+console.log(myPython);
+console.log(myJS);
