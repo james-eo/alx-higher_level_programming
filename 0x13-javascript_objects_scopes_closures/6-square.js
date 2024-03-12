02f28ce4a9737445e1cc61c1b34dda15664ff3a0
james-eo <jameseokonkwo@gmail.com>
2024-03-12 21:56:19 +0100

    Square1 inherits square0

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+class Square extends require('./5-square.js') {
+  charPrint (c) {
+    if (c === undefined) { c = 'X'; }
+    const row = c.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(row);
+    }
+  }
+}
+
+module.exports = Square;
