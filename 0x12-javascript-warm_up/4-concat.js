454f84d2beb3fd976200e17d080ff778bbe3a91a
james-eo <jameseokonkwo@gmail.com>
2024-03-12 07:55:54 +0100

    Create a sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,9 +1,18 @@
 #!/usr/bin/node
-
+/*
 const [, , arg1, arg2] = process.argv;
 
 if (arg1 === undefined || arg2 === undefined) {
   console.log('Please enter two arguments');
 } else {
   console.log(`${arg1} is ${arg2}`);
+}
+*/
+
+const args = process.argv.splice(2);
+
+if (args.length < 2) {
+    console.log("Please provide two arguments");
+} else {
+    console.log(`${args[0]} is ${args[1]}`);
 }
