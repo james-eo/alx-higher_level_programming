ca9fa0f017e2e60f6d7cddbb9b5fc60de8bdbe98
james-eo <jameseokonkwo@gmail.com>
2024-03-12 08:37:34 +0100

    Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < myArray.length; i++) {
+  console.log(myArray[i]);
+}
+
+/*
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+myArray.forEach(element => {
+    console.log(element);
+});
+
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+let i = 0;
+while (i < myArray.length) {
+    console.log(myArray[i]);
+    i++;
+}
+*/
