92437311fbeec1f7be351649386b110b2bdb12f7
james-eo <jameseokonkwo@gmail.com>
2024-03-22 23:26:20 +0100

    Filter state by args

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     cursor.execute("SELECT * FROM states WHERE name = '{}'"
-                   .format(sys.argv[4]))
+                   .format(sys.argv[4:]))
 
     rows = cursor.fetchall()
     for row in rows:
