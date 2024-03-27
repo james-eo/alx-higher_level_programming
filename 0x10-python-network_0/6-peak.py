e5663433b8048e221764ade0203e8d5d030066b3
james-eo <jameseokonkwo@gmail.com>
2024-03-27 20:15:34 +0100

    Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,6 +3,7 @@
 find a peak in a list of unsorted integers.
 """
 
+
 def find_peak(list_of_integers):
     """This function takes a list of unsorted integers as input
     and returns a peak from the list.It uses a binary search
