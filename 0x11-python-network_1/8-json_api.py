30a428439cc36f1a94487fbc957a6b5c2af0472d
james-eo <jameseokonkwo@gmail.com>
2024-03-28 22:14:09 +0100

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,21 +4,21 @@
 import requests
 import sys
 
+
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         letter = sys.argv[1]
     else:
         letter = ""
-    
+
     data = {'q': letter}
     response = requests.post("http://0.0.0.0:5000/search_user", data=data)
     try:
         json_resp = response.json()
-        
+
         if json_resp:
             print("[{}] {}".format(json_resp['id'], json_resp['name']))
         else:
             print("No result")
     except ValueError:
         print("Not a valid JSON")
-
