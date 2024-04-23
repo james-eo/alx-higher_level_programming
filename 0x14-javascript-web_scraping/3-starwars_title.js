02d2d3bae6da2921f6186352fd2b822d5a4c9263
james-eo <jameseokonkwo@gmail.com>
2024-04-23 15:36:34 +0100

    Print Star Wars title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,27 +2,13 @@
 
 const request = require('request');
 const id = process.argv[2];
-const url = 'https://swapi-api.alx-tools.com/api/films/:' + id;
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
 request.get(url, (err, response, data) => {
   if (err) {
     console.log(err);
   } else {
-    console.log(JSON.parse(data).title);
+    const movie = JSON.parse(data);
+    console.log(movie.title);
   }
 });
-
-/*
-const axios = require('axios');
-const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
-
-axios.get(url)
-  .then(response => {
-    const { title } = response.data;
-    console.log(title);
-  })
-  .catch(error => {
-    console.error("Error:", error.message);
-  });
-  */
