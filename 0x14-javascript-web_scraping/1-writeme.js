425543638b47008df27b6c14f14d21ef5839b2dc
james-eo <jameseokonkwo@gmail.com>
2024-04-23 13:34:56 +0100

    Write a string to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,10 +4,8 @@ const fs = require('fs');
 
 const filePath = process.argv[2];
 const content = process.argv[3];
-fs.writeFile(filePath, content, 'utf-8', (err) => {
+fs.writeFile(filePath, content, (err) => {
   if (err) {
-    console.error(err);
-  } else {
-    console.log();
+    console.log(err);
   }
 });
