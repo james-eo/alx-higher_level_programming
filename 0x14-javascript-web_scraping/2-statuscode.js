393e98a61068c627faeeb61f9d48be58216fa3f8
james-eo <jameseokonkwo@gmail.com>
2024-04-23 13:58:54 +0100

    Request status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,7 +3,7 @@
 const request = require('request');
 const url = process.argv[2];
 
-request(url, function (err, response, data) {
+request.get(url, (err, response, data) => {
   if (err) {
     console.log(err);
   } else {
