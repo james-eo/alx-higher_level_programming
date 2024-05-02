0987c1b072002d57b8ebc48e00d892ba5afad497
james-eo <jameseokonkwo@gmail.com>
2024-05-02 17:22:53 +0100

    JQuery Events

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,1 @@
+$(function () { $('header').css('color', '#FF0000'); });
---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,8 @@
+// document.addEventListener('DOMContentLoaded', function () {
+// document.querySelector('header').style.color = '#FF0000'
+// });
+//
+document.addEventListener('DOMContentLoaded', function () {
+  const header = document.querySelector('header');
+  header.style.color = '#FF0000';
+});
